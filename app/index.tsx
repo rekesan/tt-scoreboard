b02b4d073e71bb95e7b445bda672276d49d6c880
rekesan <redkeans@gmail.com>
2024-09-12 23:34:23 +0800

    update ui to use the new logic flow

--- app/index.tsx
+++ app/index.tsx
@@ -1,137 +1,26 @@
 import { Score } from "@/features/game/components/score";
-import { useEffect, useState } from "react";
-import { View, Text, Alert, Pressable } from "react-native";
-
-const numberOfSet = 3;
-const points2Win = 11;
-const ptsDiff = 2;
+import {
+  currentSetAtom,
+  gameState,
+  player1,
+  player2,
+  resetGame,
+} from "@/features/game/game.atoms";
+import { useAtom, useAtomValue } from "jotai";
+import { Pressable, View } from "react-native";
+import { Text } from "@/components/AnimatedText";
 
 export default function Game() {
-  const [currentSet, setCurrentSet] = useState(0);
-  const [player1, setPlayer1] = useState({
-    name: "Player 1",
-    scoresPerSet: Array.from<number>({ length: numberOfSet }).fill(0),
-    setWin: 0,
-  });
-  const [player2, setPlayer2] = useState({
-    name: "Player 2",
-    scoresPerSet: Array.from<number>({ length: numberOfSet }).fill(0),
-    setWin: 0,
-  });
-
-  let gameOver =
-    player1.setWin === Math.ceil(numberOfSet / 2) ||
-    player2.setWin === Math.ceil(numberOfSet / 2);
-
-  const moveToNextSet = () => {
-    setCurrentSet((prev) => {
-      if (prev === numberOfSet - 1) {
-        return prev;
-      }
-      return prev + 1;
-    });
-  };
-
-  useEffect(() => {
-    if (gameOver) {
-      const winner =
-        player1.setWin === Math.ceil(numberOfSet / 2) ? player1 : player2;
-      Alert.alert("Match ends", `${winner.name} wins the game!`, [
-        { text: "Reset", onPress: reset },
-      ]);
-      return;
-    }
-
-    const p1Score = player1.scoresPerSet[currentSet];
-    const p2Score = player2.scoresPerSet[currentSet];
-
-    if (p1Score >= points2Win && p1Score - p2Score >= ptsDiff) {
-      return Alert.alert(
-        "Winner",
-        `${player1.name} wins set ${currentSet + 1}!`,
-        [
-          {
-            text: "Ok",
-            onPress: () => {
-              moveToNextSet();
-              setPlayer1((prev) => ({ ...prev, setWin: prev.setWin + 1 }));
-            },
-          },
-        ]
-      );
-    }
-
-    if (p2Score >= points2Win && p2Score - p1Score >= ptsDiff) {
-      return Alert.alert(
-        "Winner",
-        `${player2.name} wins set ${currentSet + 1}!`,
-        [
-          {
-            text: "Ok",
-            onPress: () => {
-              moveToNextSet();
-              setPlayer2((prev) => ({ ...prev, setWin: prev.setWin + 1 }));
-            },
-          },
-        ]
-      );
-    }
-  }, [player1, player2, currentSet, gameOver]);
-
-  const reset = () => {
-    setCurrentSet(0);
-    setPlayer1((prev) => ({
-      ...prev,
-      scoresPerSet: Array.from<number>({ length: numberOfSet }).fill(0),
-      setWin: 0,
-    }));
-    setPlayer2((prev) => ({
-      ...prev,
-      scoresPerSet: Array.from<number>({ length: numberOfSet }).fill(0),
-      setWin: 0,
-    }));
-  };
-
   return (
-    <View className="flex h-screen w-screen flex-row justify-center items-center transition-colors dark:bg-gray-900">
-      <Score
-        score={player1.scoresPerSet.at(currentSet)}
-        playerName={player1.name}
-        className="h-screen w-1/2 bg-teal-500"
-        add={() => {
-          setPlayer1((v) => {
-            v.scoresPerSet[currentSet] += 1;
-            return { ...v };
-          });
-        }}
-        subtract={() => {
-          setPlayer1((v) => {
-            if (v.scoresPerSet[currentSet] > 0) {
-              v.scoresPerSet[currentSet] -= 1;
-            }
-            return { ...v };
-          });
-        }}
-      />
+    <View className="flex h-screen w-screen flex-row justify-center items-center transition-colors">
+      <Player atom={player1} className="h-screen w-1/2 bg-cyan-500" />
       <View className="h-full w-3 items-center bg-white z-10">
         <View className="flex-1" />
 
-        <View className="bg-white px-5 py-1 rounded-t-lg">
-          <Text className="w-24 text-center font-mono font-bold text-3xl">
-            Set {currentSet + 1}
-          </Text>
-        </View>
-        <View className="flex-row w-48 h-20 bg-white rounded-lg self-center">
-          <Text className="flex-1 text-center align-middle text-7xl leading-[1.2] font-mono font-bold text-teal-500">
-            {player1.setWin}
-          </Text>
-          <Text className="flex-1 text-center align-middle text-7xl leading-[1.2] font-mono font-bold text-amber-500">
-            {player2.setWin}
-          </Text>
-        </View>
+        <SetScore />
 
         <Pressable
-          onPress={reset}
+          onPress={resetGame}
           className="w-20 h-10 items-center justify-center active:opacity-50 bg-white rounded-full"
         >
           <Text className="text-xl leading-normal">Reset</Text>
@@ -139,25 +28,68 @@ export default function Game() {
 
         <View className="flex-[1.2]" />
       </View>
-      <Score
-        score={player2.scoresPerSet.at(currentSet)}
-        playerName={player2.name}
-        add={() => {
-          setPlayer2((v) => {
-            v.scoresPerSet[currentSet] += 1;
-            return { ...v };
-          });
-        }}
-        subtract={() => {
-          setPlayer2((v) => {
-            if (v.scoresPerSet[currentSet] > 0) {
-              v.scoresPerSet[currentSet] -= 1;
-            }
-            return { ...v };
-          });
-        }}
-        className="h-screen w-1/2 bg-amber-500"
-      />
+      <Player atom={player2} className="h-screen w-1/2 bg-rose-500" />
     </View>
   );
 }
+
+const Player = ({
+  atom,
+  className,
+}: {
+  atom: typeof player1;
+  className?: string;
+}) => {
+  const [player, setPlayer] = useAtom(atom);
+  const currentSet = useAtomValue(currentSetAtom);
+
+  return (
+    <Score
+      score={player.scoresPerSet.at(currentSet)}
+      playerName={player.name}
+      className={className}
+      add={() => {
+        setPlayer((v) => {
+          if (!v.scoresPerSet[currentSet]) {
+            v.scoresPerSet[currentSet] = 0;
+          }
+          v.scoresPerSet[currentSet] += 1;
+          return { ...v };
+        });
+      }}
+      subtract={() => {
+        setPlayer((v) => {
+          if (!v.scoresPerSet[currentSet]) {
+            v.scoresPerSet[currentSet] = 0;
+          }
+
+          if (v.scoresPerSet[currentSet] > 0) {
+            v.scoresPerSet[currentSet] -= 1;
+          }
+          return { ...v };
+        });
+      }}
+    />
+  );
+};
+
+const SetScore = () => {
+  const state = useAtomValue(gameState);
+  return (
+    <>
+      <View className="bg-white px-5 py-1 rounded-t-lg">
+        <Text className="w-24 text-center font-mono font-bold text-3xl">
+          Set {state.currentSet + 1}
+        </Text>
+      </View>
+      <View className="flex-row w-48 h-20 bg-white rounded-lg self-center">
+        <Text className="flex-1 text-center align-middle text-7xl leading-[1.2] font-mono font-bold text-cyan-500">
+          {state.p1SetScore}
+        </Text>
+        <Text className="flex-1 text-center align-middle text-7xl leading-[1.2] font-mono font-bold text-rose-500">
+          {state.p2SetScore}
+        </Text>
+      </View>
+    </>
+  );
+};
--- features/game/game.atoms.ts
+++ features/game/game.atoms.ts
@@ -1,5 +1,7 @@
-import { atom } from "jotai";
-import { selectAtom } from "jotai/utils";
+import { atom, getDefaultStore } from "jotai";
+import { Alert } from "react-native";
+
+const store = getDefaultStore();
 
 export const gameRules = atom({
   pointsToWinSet: 11,
@@ -19,34 +21,52 @@ export const player1 = atom<Player>({
 });
 
 export const player2 = atom<Player>({
-  name: "Player 1",
+  name: "Player 2",
   scoresPerSet: [],
 });
 
+export const currentSetAtom = atom(0);
+
+function incrementSet() {
+  store.set(currentSetAtom, (prev) => prev + 1);
+}
+
+const resetting = atom(false);
+
 export const gameState = atom((get) => {
   const rules = get(gameRules);
+  const currentSet = get(currentSetAtom);
   const p1 = get(player1);
   const p2 = get(player2);
 
   const numOfSets2Win = Math.ceil(rules.numOfSets / 2);
   let p1SetScore = 0;
   let p2SetScore = 0;
+  let currentSetWinner: Player | null = null;
 
   for (let i = 0; i < rules.numOfSets; i++) {
-    const p1Score = p1.scoresPerSet[i];
-    const p2Score = p2.scoresPerSet[i];
+    const p1Score = p1.scoresPerSet[i] || 0;
+    const p2Score = p2.scoresPerSet[i] || 0;
+
+    if (store.get(resetting)) continue;
 
     if (
       p1Score >= rules.pointsToWinSet &&
       p1Score - p2Score >= rules.setPointsDiff2Win
     ) {
       p1SetScore += 1;
+      if (i === currentSet) {
+        currentSetWinner = p1;
+      }
     }
     if (
       p2Score >= rules.pointsToWinSet &&
       p2Score - p1Score >= rules.setPointsDiff2Win
     ) {
       p2SetScore += 1;
+      if (i === currentSet) {
+        currentSetWinner = p2;
+      }
     }
   }
 
@@ -62,11 +82,29 @@ export const gameState = atom((get) => {
     isGameOver = true;
   }
 
+  if (isGameOver && winner) {
+    Alert.alert("Game over!", `${winner.name} wins the match!`);
+  } else if (currentSetWinner) {
+    Alert.alert(
+      "Set Win",
+      `${currentSetWinner.name} wins set ${currentSet + 1}!`,
+      [{ text: "Next", onPress: incrementSet }]
+    );
+  }
+
   return {
-    currentSet: 0,
+    currentSet,
     isGameOver,
-    winner
+    winner,
+    p1SetScore,
+    p2SetScore,
   };
 });
 
-export const isGameOver = selectAtom(gameState, (v) => v.isGameOver);
+export function resetGame() {
+  store.set(resetting, true);
+  store.set(player1, (prev) => ({ ...prev, scoresPerSet: [] }));
+  store.set(player2, (prev) => ({ ...prev, scoresPerSet: [] }));
+  store.set(currentSetAtom, 0);
+  store.set(resetting, false);
+}
