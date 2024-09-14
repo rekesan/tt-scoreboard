ad8bcce62e3ba089b9af0a67c860f7abcf18eec9
rekesan <redkeans@gmail.com>
2024-09-14 15:52:19 +0800

    replace Text cssInterop to a named component
    see: https://www.nativewind.dev/v4/guides/third-party-components#improving-3rd-party-components

--- components/AnimatedText.tsx
+++ components/AnimatedText.tsx
@@ -1,9 +1,9 @@
-import { cssInterop } from "nativewind";
+import { ComponentProps } from "react";
 import { TextInput as TI } from "react-native";
 import Animated from "react-native-reanimated";
 
-export const Text = cssInterop(Animated.Text, {
-  className: { target: "style" },
-});
+export const Text = (props: ComponentProps<Animated.Text>) => {
+  return <Animated.Text {...props} />;
+};
 
 export const TextInput = Animated.createAnimatedComponent(TI);
